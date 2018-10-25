439ee2f900532bbd54b136d9ed5bc9d06a0e484f
Villoro <villoro7@gmail.com>
2018-10-25 19:08:54 +0200

    [NEW] navbar added

--- src/index.py
+++ src/index.py
@@ -1,12 +1,12 @@
 """ Flask app index """
 
-from flask import Flask
+from flask import Flask, render_template
 
 APP = Flask(__name__)
 
 @APP.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template("base.html")
 
 
 if __name__ == '__main__':
