01219ddf80092043790b3d7b28876df300e69908
Villoro <villoro7@gmail.com>
2018-10-26 12:18:57 +0200

    [NEW] use page template for each page (except post and portfolio)

--- src/index.py
+++ src/index.py
@@ -5,8 +5,13 @@ from flask import Flask, render_template
 APP = Flask(__name__)
 
 @APP.route('/')
-def hello_world():
+@APP.route('/home')
+def home():
     return render_template("home.html")
+
+@APP.route('/about')
+def about():
+    return render_template("about.html")
 
 
 if __name__ == '__main__':
