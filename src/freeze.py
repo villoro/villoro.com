1998c1fcbff2ceb2c2b9f7570e2e06fc81164203
Villoro <villoro7@gmail.com>
2018-11-04 21:13:03 +0100

    [NEW] define custom link colors + allow freezer preview with sys arg

--- src/freeze.py
+++ src/freeze.py
@@ -1,10 +1,26 @@
 """ Flask freezer """
 
+import sys
+import os # TODO: also temporal
+
 from flask_frozen import Freezer
 from index import APP
 
 FREEZER = Freezer(APP)
 
+# TODO: this is temporal
+# Generate portfolio pages
+@FREEZER.register_generator
+def portfolio_items():
+    for name in os.listdir("src/portfolio"):
+        yield '/portfolio-items/{}.html'.format(name.split(".")[0])
+
 
 if __name__ == '__main__':
-    FREEZER.freeze()
+
+    if "preview" == sys.argv[1]:
+        print("Running Flask-frozen preview mode")
+        FREEZER.run(debug=True)
+
+    else:
+        FREEZER.freeze()
