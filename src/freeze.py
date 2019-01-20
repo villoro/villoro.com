46627fbd288e75cf1866a3f2d25409bd642f6214
Villoro <villoro7@gmail.com>
2019-01-20 20:12:16 +0100

    Merge branch 'r/1.2.0'

--- src/freeze.py
+++ src/freeze.py
@@ -1,8 +1,5 @@
 """ Flask freezer """
 
-import sys
-import os # TODO: also temporal
-
 from flask_frozen import Freezer
 from index import APP
 
@@ -10,10 +7,4 @@ FREEZER = Freezer(APP)
 
 
 if __name__ == '__main__':
-
-    if (len(sys.argv) > 1) and ("preview" == sys.argv[1]):
-        print("Running Flask-frozen preview mode")
-        FREEZER.run(debug=True)
-
-    else:
-        FREEZER.freeze()
+    FREEZER.freeze()
