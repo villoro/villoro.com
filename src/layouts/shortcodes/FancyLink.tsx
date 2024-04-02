db72fdff1c03a4df9bd03575369e78063583bebe
Arnau Villoro <arnau@villoro.com>
2024-04-02 20:17:27 +0200

    Allow to manually set the favicon

--- src/layouts/shortcodes/FancyLink.tsx
+++ src/layouts/shortcodes/FancyLink.tsx
@@ -3,14 +3,16 @@ import React from "react";
 interface FancyLinkProps {
   url: string;
   linkText: string;
+  faviconUrl?: string; // Optional favicon URL
 }
 
-const FancyLink: React.FC<FancyLinkProps> = ({ url, linkText }) => {
-  const domainLogoUrl = `${url.split('//')[0]}//${url.split('/')[2]}/favicon.ico`;
+const FancyLink: React.FC<FancyLinkProps> = ({ url, linkText, faviconUrl }) => {
+  const domainLogoUrl = faviconUrl || `${url.split('//')[0]}//${url.split('/')[2]}/favicon.ico`;
 
   return (
-    <a href={url} role="button" className="inline-flex rounded-md border border-gray-300 px-2 flex items-center hover:bg-gray-100">
-      <img src={domainLogoUrl} alt="Domain Logo" className="w-4 h-4 mr-2 my-0" />
+    <a href={url} role="button" className="inline-flex rounded-md border border-gray-300 px-2 items-center hover:bg-gray-100">
+      {faviconUrl && <img src={faviconUrl} alt="Domain Logo" className="w-4 h-4 mr-2 my-0" />}
+      {!faviconUrl && <img src={domainLogoUrl} alt="Domain Logo" className="w-4 h-4 mr-2 my-0" />}
       <span className="text-primary dark:text-darkmode-primary">{linkText}</span>
     </a>
   );
