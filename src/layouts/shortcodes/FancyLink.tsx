4402f41e2dbffc6100c0f8baf9c3aa6937c0c311
Arnau Villoro <arnau@villoro.com>
2024-04-02 20:07:28 +0200

    Infer favicon.ico + fix style + use it in posts

--- src/layouts/shortcodes/FancyLink.tsx
+++ src/layouts/shortcodes/FancyLink.tsx
@@ -2,13 +2,14 @@ import React from "react";
 
 interface FancyLinkProps {
   url: string;
-  domainLogoUrl: string;
   linkText: string;
 }
 
-const FancyLink: React.FC<FancyLinkProps> = ({ url, domainLogoUrl, linkText }) => {
+const FancyLink: React.FC<FancyLinkProps> = ({ url, linkText }) => {
+  const domainLogoUrl = `${url.split('//')[0]}//${url.split('/')[2]}/favicon.ico`;
+
   return (
-    <a href={url} role="button" className="inline-block rounded-md border border-gray-300 px-2 flex items-center hover:bg-gray-100">
+    <a href={url} role="button" className="inline-flex rounded-md border border-gray-300 px-2 flex items-center hover:bg-gray-100">
       <img src={domainLogoUrl} alt="Domain Logo" className="w-4 h-4 mr-2 my-0" />
       <span className="text-primary dark:text-darkmode-primary">{linkText}</span>
     </a>
