5047844b02d5d5942565f81e361d484c42078d23
Arnau Villoro <arnau@villoro.com>
2024-04-03 08:17:14 +0200

    Switch to self hosted icons

--- src/layouts/shortcodes/FancyLink.tsx
+++ src/layouts/shortcodes/FancyLink.tsx
@@ -3,16 +3,28 @@ import React from "react";
 interface FancyLinkProps {
   url: string;
   linkText: string;
-  faviconUrl?: string; // Optional favicon URL
+  company?: string; // Optional custom company name
 }
 
-const FancyLink: React.FC<FancyLinkProps> = ({ url, linkText, faviconUrl }) => {
-  const domainLogoUrl = faviconUrl || `${url.split('//')[0]}//${url.split('/')[2]}/favicon.ico`;
+const FancyLink: React.FC<FancyLinkProps> = ({ url, linkText, company }) => {
+  const extractCompanyName = (url: string): string => {
+    const domain = new URL(url).hostname;
+    const parts = domain.split('.');
+    if (company) {
+      return company;
+    } else if (parts.length > 2) {
+      return parts[parts.length - 2];
+    } else {
+      return parts[0];
+    }
+  };
+
+  const companyName = extractCompanyName(url);
+  const domainLogoUrl = `/favicons/${companyName}.ico`;
 
   return (
     <a href={url} role="button" className="inline-flex rounded-md border border-gray-300 px-2 items-center hover:bg-gray-100">
-      {faviconUrl && <img src={faviconUrl} alt="Domain Logo" className="w-4 h-4 mr-2 my-0" />}
-      {!faviconUrl && <img src={domainLogoUrl} alt="Domain Logo" className="w-4 h-4 mr-2 my-0" />}
+      <img src={domainLogoUrl} alt="Domain Logo" className="w-4 h-4 mr-2 my-0" />
       <span className="text-primary dark:text-darkmode-primary">{linkText}</span>
     </a>
   );
