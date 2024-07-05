df3219154192fdaebff1ed51c98513a88bd9b949
Arnau Villoro <arnau@villoro.com>
2024-07-05 08:51:09 +0200

    Show fancy categories

---
+++ src/lib/categoryMetadata.ts
@@ -0,0 +1,10 @@
+export const categoryMetadata = {
+  python: { name: "🐍 Python" },
+  web: { name: "🌍 Web" },
+  DE: { name: "⚙️ DE" },
+  cloud_devops: { name: "☁️ Cloud/DevOps" },
+  tools: { name: "🛠️ Tools/Utils" },
+  AI: { name: "🤖 ML/AI" },
+  git: { name: "🔀 GIT" },
+  others: { name: "📦 Others" }
+};
