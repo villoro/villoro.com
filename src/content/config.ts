781617a55f78ba3292fd941c959c10cc80b13955
Arnau Villoro <arnau@villoro.com>
2025-12-21 11:03:31 +0100

    Remove unused collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,5 @@
 import { defineCollection, z } from "astro:content";
 
-// Post collection schema
 const blogCollection = defineCollection({
   type: "content",
   schema: z.object({
@@ -12,30 +11,6 @@ const blogCollection = defineCollection({
     author: z.string().default("Admin"),
     category: z.string().default("Others"),
     tags: z.array(z.string()).default(["Others"]),
-    draft: z.boolean().optional(),
-  }),
-});
-
-// Author collection schema
-const authorsCollection = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    meta_title: z.string().optional(),
-    email: z.string().optional(),
-    image: z.string().optional(),
-    description: z.string().optional(),
-    social: z
-      .array(
-        z
-          .object({
-            name: z.string().optional(),
-            icon: z.string().optional(),
-            link: z.string().optional(),
-          })
-          .optional(),
-      )
-      .optional(),
     draft: z.boolean().optional(),
   }),
 });
@@ -98,23 +73,9 @@ const homepageCollection = defineCollection({
   }),
 });
 
-// Pages collection schema
-const pagesCollection = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    meta_title: z.string().optional(),
-    description: z.string().optional(),
-    image: z.string().optional(),
-    draft: z.boolean().optional(),
-  }),
-});
-
 // Export collections
 export const collections = {
   blog: blogCollection,
-  authors: authorsCollection,
-  pages: pagesCollection,
   about: aboutCollection,
   homepage: homepageCollection,
 };
