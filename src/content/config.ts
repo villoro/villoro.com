74a0a7211d2d2729f4286410c214aa1e0e5d8dda
Arnau Villoro <arnau@villoro.com>
2025-12-21 10:43:30 +0100

    Improve config

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,6 +2,7 @@ import { defineCollection, z } from "astro:content";
 
 // Post collection schema
 const blogCollection = defineCollection({
+  type: "content",
   schema: z.object({
     title: z.string(),
     meta_title: z.string().optional(),
@@ -17,6 +18,7 @@ const blogCollection = defineCollection({
 
 // Author collection schema
 const authorsCollection = defineCollection({
+  type: "content",
   schema: z.object({
     title: z.string(),
     meta_title: z.string().optional(),
@@ -38,8 +40,67 @@ const authorsCollection = defineCollection({
   }),
 });
 
+const aboutCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    meta_title: z.string().optional(),
+    description: z.string().optional(),
+    date: z.date().optional(),
+    image: z.string().optional(),
+    draft: z.boolean().optional(),
+
+    info: z
+      .object({
+        title: z.string().optional(),
+        imageAlt: z.string().optional(),
+      })
+      .optional(),
+  }),
+});
+
+const homepageCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    banner: z.object({
+      image: z.string().optional(),
+      title: z.string(),
+      content: z.string().optional(),
+    }),
+
+    slogan: z
+      .object({
+        title: z.string().optional(),
+      })
+      .optional(),
+
+    blog: z
+      .object({
+        title: z.string().optional(),
+        button: z.string().optional(),
+      })
+      .optional(),
+
+    companies: z
+      .object({
+        title: z.string().optional(),
+        logos: z
+          .array(
+            z.object({
+              name: z.string(),
+              image: z.string().optional(),
+              link: z.string().url().optional(),
+            }),
+          )
+          .optional(),
+      })
+      .optional(),
+  }),
+});
+
 // Pages collection schema
 const pagesCollection = defineCollection({
+  type: "content",
   schema: z.object({
     title: z.string(),
     meta_title: z.string().optional(),
@@ -54,4 +115,6 @@ export const collections = {
   blog: blogCollection,
   authors: authorsCollection,
   pages: pagesCollection,
+  about: aboutCollection,
+  homepage: homepageCollection,
 };
