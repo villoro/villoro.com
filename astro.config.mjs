4f7143dfb697319836dabaa37686bfb534bddacb
Arnau Villoro <arnau@villoro.com>
2026-05-06 09:05:15 +0200

    Minor improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,17 +32,19 @@ export default defineConfig({
         start_url: "/",
         display: "standalone",
         theme_color: "#3a76ff",
-        background_color: "#f6f6f6",
+        background_color: "#3a76ff",
         icons: [
           {
             src: "/pwa/pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
+            purpose: "any",
           },
           {
             src: "/pwa/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
+            purpose: "any",
           },
           {
             src: "/pwa/pwa-maskable-512x512.png",
